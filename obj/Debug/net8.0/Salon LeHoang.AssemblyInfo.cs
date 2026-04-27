51d1ae2cca515b50af0f410377cbf8cecf7f9a2e
Phúc <trantrongphuc28042005@gmail.com>
2026-04-27 20:32:03 +0700

    fix lỗi lặt vặt

--- obj/Debug/net8.0/Salon LeHoang.AssemblyInfo.cs
+++ obj/Debug/net8.0/Salon LeHoang.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Salon LeHoang")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+433f6437e5a6f79c93f8724d4de751d4cb4a9fb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b80b5c9abf4998efba74b338f17a06ef3af49fb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Salon LeHoang")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Salon LeHoang")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
